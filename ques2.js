565a448bce2160e7a0ee0ad86173847129a9374f
anurag2427 <35267869+anurag2427@users.noreply.github.com>
2018-10-29 16:15:13 +0530

    Add files via upload

---
+++ ques2.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+function ReadAppend(file, appendFile){
+  fs.readFile(appendFile, function (err, data) {
+    if (err) throw err;
+    console.log('File was read');
+
+    fs.appendFile(file, data, function (err) {
+      if (err) throw err;
+      console.log('The \"data to append\" was appended to file!');
+
+    });
+  });
+}
+// edit this with your file names
+file = 'text1.txt';
+appendFile = 'text2.txt';
+ReadAppend(file, appendFile);
